2e4dc3dd70aecd3d73b3c839d291824a4baa30ea
Paul-Michael <paulmichael.schreiber@gmail.com>
2016-10-31 14:08:45 -0500

    adds functionality for testing with mocha/chai

---
+++ test/App.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+const React = require('react')
+const chai = require('chai')
+const { expect } = chai
+const enzyme = require('enzyme')
+const { render } = enzyme
+const ReactRedux = require('react-redux')
+const { Provider } = ReactRedux
+
+
+
